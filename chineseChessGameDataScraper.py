ef56397237d8bff3b877bcca8bbd33387a81aae8
changhsinlee <casper5866@gmail.com>
2017-02-10 12:02:06 -0600

    add basic functions

--- chineseChessGameDataScraper.py
+++ chineseChessGameDataScraper.py
@@ -1,10 +1,33 @@
 #! python3
+# chineseChessGameDataScraper.py -
 # Downloads chinese chess (xiangqi) data from playok.com into .txt files
-
-# TODO: Open corresponding page
 # example: http://www.playok.com/zh/game.phtml/57390680.txt?xq
 
-# TODO: Save content of page into corresponding text file
+import os, requests
+
+# set working directory to where script is
+dataPath = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data')
+os.chdir(dataPath)
+
+start = 57390680
+end = 57390690
+
+gameList = list(range(start,end))
+
+for gameID in gameList:
+    # open corresponding page
+    targetURL = f'http://www.playok.com/zh/game.phtml/{str(gameID)}.txt?xq'
+    res = requests.get(targetURL)
+    res.raise_for_status()
+    
+    # Save content of page into corresponding text file
+    gameFileName = f'{str(gameID)}.txt'
+    gameFile = open(gameFileName, 'wb')
+    for chunk in res.iter_content(100000):
+        gameFile.write(chunk)
+        
+    gameFile.close()
+
 
 # TODO: Data wrangling option
 
