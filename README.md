169c5a6d0ced19bb33f9426cd67cc86b9d891cca
changhsinlee <casper5866@gmail.com>
2017-02-10 09:28:29 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Scrapes xiangqi (chinese chess) data from playok.com into .txt files on the local machine by game. Python 3 required.
---
+++ chineseChessGameDataScraper.py
@@ -0,0 +1,11 @@
+#! python3
+# Downloads chinese chess (xiangqi) data from playok.com into .txt files
+
+# TODO: Open corresponding page
+# example: http://www.playok.com/zh/game.phtml/57390680.txt?xq
+
+# TODO: Save content of page into corresponding text file
+
+# TODO: Data wrangling option
+
+# TODO: Estimate file size?
